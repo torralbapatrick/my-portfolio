bbec794c1a1d5a3d89fb9e6c4e2b10887779a0ca
Patrick <torralbapatrick15@gmail.com>
2019-01-03 17:13:55 -0600

    Update whole content

---
+++ js/app.js
@@ -0,0 +1,17 @@
+jQuery(document).ready(function($) {
+    $('.level-bar-inner').css('width', '0');
+
+    $('.level-bar-inner').each(function() {
+    
+        var itemWidth = $(this).data('level');
+
+        $(this).animate({
+            width: itemWidth
+        }, 800);
+        
+    });
+
+    
+    /* Bootstrap Tooltip for Skillset */
+    $('.level-label').tooltip();
+});
